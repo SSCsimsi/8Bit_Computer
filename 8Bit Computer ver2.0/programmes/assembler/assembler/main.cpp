0b3ccedbcb1964e4d5ee1f0e839e5a0b24eb2ac0
Silberbauer Florian <Florian.Silberbauer@htl-wels.at>
2026-01-29 10:19:02 +0100

    not fully working yet
    the values it writes to the files are not correct

--- 8Bit Computer ver2.0/programmes/assembler/assembler/main.cpp
+++ 8Bit Computer ver2.0/programmes/assembler/assembler/main.cpp
@@ -1,13 +1,59 @@
 
-#include "main.h"
+#include <stdint.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+#define CMDLENGTH 3
+
+void CreateList();
+
+class Cmd {
+public:
+
+	string name;
+	uint8_t val;
+
+	/*
+	 * the name is made up of 3 ascii symbols,
+	 * the value has to fit within 8 bits
+	 */
+	void Init(string nameIn, uint8_t valIn) {
+		name = nameIn;
+		val = valIn;
+	}
+
+	uint8_t Compare(string newLine) {
+		return name == newLine.substr(0, CMDLENGTH);
+	}
+
+	uint8_t Put() {
+		return val;
+	}
+
+
+private:
+
+};
+
+list<Cmd> Commands;
 
 int main() {
 
-	FILE *fptrCode;
+	Cmd Nop;
+	Nop.Init("nop", 0x00);
+	Commands.push_back(Nop);
 
-	FILE *fptrCmd;
-	FILE *fptrDat;
-	FILE *fptrAdr;
+	Cmd Jump;
+	Jump.Init("jmp", 0x01);
+	Commands.push_back(Jump);
+
+	Cmd JumpIf;
+	JumpIf.Init("cjp", 0x02);
+	Commands.push_back(JumpIf);
 
 	/* Open the code */
 	ifstream codeFile("code.txt");
@@ -17,21 +63,44 @@ int main() {
 	ofstream adrFile("adr.hex");
 
 	string newLine;
-	CreateList();
+	uint8_t check;
 
-	getline(codeFile, newLine);
-	for(Cmd cmd : Commands) {
-		if(cmd.Compare(newLine)) {
-			cmdFile << cmd.Put();
-			//TODO: convert string to binary
-			cmdFile << newLine.substr(4, 5);
+	while(getline(codeFile, newLine)) {
+
+		check = 0;
+
+		for(Cmd cmd : Commands) {
+			if(cmd.Compare(newLine)) {
+
+				cmdFile << cmd.Put();
+				check = 1;
+
+				break;
+			}
 		}
 
-	}
+		if(!check) {
+			ofstream Error("UnknownCommand.txt");
+			Error.close();
 
+			return -1;
+		}
+
+		cmdFile << (uint8_t)stoi(newLine.substr(4, 5)); //value changed from string to int
+		adrFile << (uint8_t)stoi(newLine.substr(7,10));
+
+		getline(codeFile, newLine);
+	}
 
 	/* Close the files */
 	codeFile.close();
 	cmdFile.close();
 	adrFile.close();
+}
+
+
+
+void CreateList() {
+
+
 }
