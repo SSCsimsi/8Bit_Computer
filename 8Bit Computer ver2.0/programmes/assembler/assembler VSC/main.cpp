702d0e1b27e6305c3b62445cc781fc8526bd6fbd
Schmiddi <133159820+SSCsimsi@users.noreply.github.com>
2026-03-06 16:12:31 +0100

    prolonged address in flash for sto

--- 8Bit Computer ver2.0/programmes/assembler/assembler VSC/main.cpp
+++ 8Bit Computer ver2.0/programmes/assembler/assembler VSC/main.cpp
@@ -24,6 +24,11 @@ enum Disables{
     AddressBus
 };
 
+enum Duration{
+    oneCycle,
+    twoCycles
+};
+
 class Cmd {
 public:
 
@@ -31,11 +36,12 @@ public:
 	 * the name is made up of 3 ascii symbols,
 	 * the value has to fit within 8 bits
 	 */
-	void Init(string nameIn, uint8_t valIn, Requires reqIn, Disables disIn) {
+	void Init(string nameIn, uint8_t valIn, Requires reqIn, Duration durIn, Disables disIn) {
 		name = nameIn;
 		val = valIn;
         req = reqIn;
         dis = disIn;
+        dur = durIn;
 	}
 
 	string Name() {
@@ -53,12 +59,17 @@ public:
     Disables Dis() {
         return dis;
     }
+    
+    Duration Dur() {
+        return dur;
+    }
 
 private:
     string name;
 	uint8_t val;
     Requires req;
     Disables dis;
+    Duration dur;
 };
 
 list<Cmd> Commands;
@@ -86,6 +97,9 @@ int main() {
     uint16_t currentLine = 0;
 
     Disables prevBusAccess = None;
+
+    uint16_t prevAdr = 0;
+    Duration prevAdrDuration = oneCycle;
 
 	while(getline(codeFile, newLine)) {
 
@@ -131,7 +145,7 @@ int main() {
                 /* cmd -- XXXX */
                 uint16_t adr = 0x0000;
 
-                /* only if the command needs an address, and no previous command is outputting an address to the bus */
+                /* if the command needs an address, and no previous command is outputting an address to the bus */
                 if((cmd.Req(Requires::Address) or cmd.Req(Requires::Both)) and !(prevBusAccess == Disables::AddressBus)) {
 
                     /* try to write 16bits to the adr file */
@@ -139,17 +153,24 @@ int main() {
                         newLine = newLine.substr(newLine.find(' ') + 1);
                         string s = newLine.substr(0, 4);
                         adr = stoul(s, nullptr, 16);
+                        prevAdr = adr;
                     } 
                     catch(exception e) {
                         errorLog << "unknown address value at line: " << ToHex(currentLine) << endl;
                     }
                 }
+
+                /* in order to keep addresses on for one cycle longer */
+                if(prevAdrDuration == twoCycles) {
+                    adr = prevAdr;
+                }
                 adrFile.write(reinterpret_cast<char*>(&adr), 2);
 
 
-
+                
                 currentLine++;
                 prevBusAccess = cmd.Dis();
+                prevAdrDuration = cmd.Dur();
 
                 check = 1;
                 break;
@@ -175,71 +196,71 @@ void CreateList() {
     Cmd cmd;
 
     // --- CORE ---
-    cmd.Init("nop", 0x00, Requires::Nothing, Disables::None);       Commands.push_back(cmd); // No Operation
-    cmd.Init("jmp", 0x01, Requires::Address, Disables::None);       Commands.push_back(cmd); // Jump
-    cmd.Init("cjp", 0x02, Requires::Both,    Disables::None);       Commands.push_back(cmd); // Jump if
+    cmd.Init("nop", 0x00, Requires::Nothing, Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // No Operation
+    cmd.Init("jmp", 0x01, Requires::Address, Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Jump
+    cmd.Init("cjp", 0x02, Requires::Both,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Jump if
 
     // --- BUS TRANSFER ---
-    cmd.Init("stx", 0x03, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store to interface reg 1
-    cmd.Init("sty", 0x04, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store to interface reg 2
-    cmd.Init("ldz", 0x05, Requires::Nothing, Disables::AddressBus); Commands.push_back(cmd); // Load interface regs
+    cmd.Init("stx", 0x03, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store to interface reg 1
+    cmd.Init("sty", 0x04, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store to interface reg 2
+    cmd.Init("ldz", 0x05, Requires::Nothing, Duration::oneCycle, Disables::AddressBus); Commands.push_back(cmd); // Load interface regs
 
     // --- STORAGE ---
-    cmd.Init("sto", 0x06, Requires::Both,    Disables::None);       Commands.push_back(cmd); // Store to RAM
-    cmd.Init("lod", 0x07, Requires::Address, Disables::DataBus);    Commands.push_back(cmd); // Load from RAM
+    cmd.Init("sto", 0x06, Requires::Both,    Duration::oneCycle,  Disables::None);      Commands.push_back(cmd); // Store to RAM
+    cmd.Init("lod", 0x07, Requires::Address, Duration::twoCycles, Disables::DataBus);   Commands.push_back(cmd); // Load from RAM
 
     // --- ALU ---
-    cmd.Init("add", 0x08, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Add
-    cmd.Init("adc", 0x09, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Add with carry
-    cmd.Init("sub", 0x0A, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Subtract
+    cmd.Init("add", 0x08, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Add
+    cmd.Init("adc", 0x09, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Add with carry
+    cmd.Init("sub", 0x0A, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Subtract
 
     // --- INCREMENTOR ---
-    cmd.Init("inc", 0x0B, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Increment
-    cmd.Init("dec", 0x0C, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Decrement
+    cmd.Init("inc", 0x0B, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Increment
+    cmd.Init("dec", 0x0C, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Decrement
 
     // --- LOGIC ---
-    cmd.Init("not", 0x0D, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Binary Not
-    cmd.Init("orr", 0x0E, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Binary Or
-    cmd.Init("and", 0x0F, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Binary And
-    cmd.Init("xor", 0x10, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Binary Xor
-    cmd.Init("bnt", 0x11, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Bitwise Not
-    cmd.Init("bor", 0x12, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Bitwise Or
-    cmd.Init("ban", 0x13, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Bitwise And
-    cmd.Init("bxr", 0x14, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Bitwise Xor
+    cmd.Init("not", 0x0D, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Binary Not
+    cmd.Init("orr", 0x0E, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Binary Or
+    cmd.Init("and", 0x0F, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Binary And
+    cmd.Init("xor", 0x10, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Binary Xor
+    cmd.Init("bnt", 0x11, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Bitwise Not
+    cmd.Init("bor", 0x12, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Bitwise Or
+    cmd.Init("ban", 0x13, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Bitwise And
+    cmd.Init("bxr", 0x14, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Bitwise Xor
 
     // --- COMPARE ---
-    cmd.Init("cgt", 0x15, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Compare Greater than
-    cmd.Init("ceq", 0x16, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Compare Equal
-    cmd.Init("clt", 0x17, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Compare Smaller than
+    cmd.Init("cgt", 0x15, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Compare Greater than
+    cmd.Init("ceq", 0x16, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Compare Equal
+    cmd.Init("clt", 0x17, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Compare Smaller than
 
     // --- A/B/C REGISTERS ---
-    cmd.Init("sta", 0x18, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store A
-    cmd.Init("lda", 0x19, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load A
-    cmd.Init("stb", 0x1A, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store B
-    cmd.Init("ldb", 0x1B, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load B
-    cmd.Init("stc", 0x1C, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store C
-    cmd.Init("ldc", 0x1D, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load C
+    cmd.Init("sta", 0x18, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store A
+    cmd.Init("lda", 0x19, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load A
+    cmd.Init("stb", 0x1A, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store B
+    cmd.Init("ldb", 0x1B, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load B
+    cmd.Init("stc", 0x1C, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store C
+    cmd.Init("ldc", 0x1D, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load C
 
     // --- FLAG REGISTER ---
-    cmd.Init("stf", 0x1E, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store Flags
-    cmd.Init("lcr", 0x1F, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load Carry
-    cmd.Init("ldo", 0x20, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load Overflow
-    cmd.Init("ldf", 0x21, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load Flags
+    cmd.Init("stf", 0x1E, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store Flags
+    cmd.Init("lcr", 0x1F, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load Carry
+    cmd.Init("ldo", 0x20, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load Overflow
+    cmd.Init("ldf", 0x21, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load Flags
 
     // --- SHIFT REGISTER ---
-    cmd.Init("std", 0x22, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store Distance
-    cmd.Init("ldd", 0x23, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load Distance
-    cmd.Init("sft", 0x24, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Shift
-    cmd.Init("sfc", 0x25, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Shift Circular
+    cmd.Init("std", 0x22, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store Distance
+    cmd.Init("ldd", 0x23, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load Distance
+    cmd.Init("sft", 0x24, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Shift
+    cmd.Init("sfc", 0x25, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Shift Circular
 
     // --- INPUTS ---
-    cmd.Init("sti", 0x26, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store Inputs
-    cmd.Init("ldi", 0x27, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load Inputs
-    cmd.Init("ldu", 0x28, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load Unread
+    cmd.Init("sti", 0x26, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store Inputs
+    cmd.Init("ldi", 0x27, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load Inputs
+    cmd.Init("ldu", 0x28, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load Unread
 
     // --- OUTPUTS ---
-    cmd.Init("sop", 0x29, Requires::Data,    Disables::None);       Commands.push_back(cmd); // Store Outputs
-    cmd.Init("lop", 0x2A, Requires::Nothing, Disables::DataBus);    Commands.push_back(cmd); // Load Outputs
+    cmd.Init("sop", 0x29, Requires::Data,    Duration::oneCycle, Disables::None);       Commands.push_back(cmd); // Store Outputs
+    cmd.Init("lop", 0x2A, Requires::Nothing, Duration::oneCycle, Disables::DataBus);    Commands.push_back(cmd); // Load Outputs
 
 
 
